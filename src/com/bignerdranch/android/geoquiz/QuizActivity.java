2d9d1c0eeaf68be606d307f00b1afcb852ec5f88
Russell M Zauner <russellmzauner@gmail.com>
2014-07-08 12:19:32 -0700

    Code that crashes
    An update that actually tries to run.  displays a message on emulator
    that app has crashed.

---
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,41 @@
+package com.bignerdranch.android.geoquiz;
+
+//import android.R.menu;
+import android.app.Activity;
+import android.os.Bundle;
+//import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends Activity{
+	
+	private Button mTrueButton;
+	private Button mFalseButton;
+
+	public void OnCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quiz);
+		
+		mTrueButton = (Button)findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+			}
+		});
+		mFalseButton = (Button)findViewById(R.id.false_button);
+		
+		mFalseButton.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+	
+/*	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_quiz, menu);
+		return true;
+	}*/
+}
